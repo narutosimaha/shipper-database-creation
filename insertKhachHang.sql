27b18c9f13c07ad236b28edb0c67487be173b04e
Jax Vu <82920386+narutosimaha@users.noreply.github.com>
2021-11-20 14:50:20 +0700

    Create insertKhachHang.sql

---
+++ insertKhachHang.sql
@@ -0,0 +1,14 @@
+INSERT INTO KhachHang(ho,tenLot,Ten,gioiTinh,taiKhoan,matKhau,CCCDorVisa) 
+VALUES ('Tran', 'Luong', 'Vu',1,'narutosimha','test1234',12344321)
+
+INSERT INTO KhachHang(ho,tenLot,Ten,gioiTinh,taiKhoan,matKhau,CCCDorVisa) 
+VALUES ('Nguyen', 'Van', 'Thuong',1,'thuongnguyen123','test1234',23455432)
+
+INSERT INTO KhachHang(ho,tenLot,Ten,gioiTinh,taiKhoan,matKhau,CCCDorVisa) 
+VALUES ('Nguyen', 'Le', 'Hien',1,'lehien123','test1234',34566543)
+
+INSERT INTO KhachHang(ho,tenLot,Ten,gioiTinh,taiKhoan,matKhau,CCCDorVisa) 
+VALUES ('Luu', 'Cong', 'Dinh',1,'congdinh123','test1234',45677654)
+
+INSERT INTO KhachHang(ho,tenLot,Ten,gioiTinh,taiKhoan,matKhau,CCCDorVisa) 
+VALUES ('Nguyen', 'Quoc', 'Thai',1,'quocthai123','test1234',56788765)
